e350d9a68eb933098a49d0faaf5aebdf39a2d70f
Adriankv1 <adriankv@uia.no>
2023-02-20 01:16:21 +0100

    update

--- main.go
+++ main.go
@@ -3,92 +3,52 @@ package main
 import (
 	"flag"
 	"fmt"
-
 	"funtemps/v2/conv"
 )
 
-var (
-	f   float64
-	c   float64
-	k   float64
-	out string
-	t   string
-)
+var from string
+var to string
+var temp float64
 
 func init() {
-	flag.Float64Var(&f, "F", 0.0, "temperature in degrees Fahrenheit")
-	flag.Float64Var(&c, "C", 0.0, "temperature in degrees Celsius")
-	flag.Float64Var(&k, "K", 0.0, "temperature in Kelvin")
-	flag.StringVar(&out, "out", "C", "output temperature scale: C - Celsius, F - Fahrenheit, K - Kelvin")
-	flag.StringVar(&t, "t", "C", "temperature scale for fun facts: C - Celsius, F - Fahrenheit, K - Kelvin")
+	flag.StringVar(&from, "from", "C", "source temperature scale (C/F/K)")
+	flag.StringVar(&to, "to", "F", "target temperature scale (C/F/K)")
+	flag.Float64Var(&temp, "temp", 0, "temperature to convert")
 }
 
 func main() {
 	flag.Parse()
 
-	numTempFlags := 0
-	if f != 0 {
-		numTempFlags++
-	}
-	if c != 0 {
-		numTempFlags++
-	}
-	if k != 0 {
-		numTempFlags++
-	}
-	if numTempFlags != 1 {
-		fmt.Println("Please specify exactly one of the temperature flags (-F, -C, -K)")
-		return
-	}
+	var result float64
 
-	var converted float64
-	switch {
-	case f != 0:
-		switch out {
-		case "C":
-			converted = conv.FahrenheitToCelsius(f)
-		case "K":
-			converted = conv.FahrenheitToKelvin(f)
-		case "F":
-			converted = f
-		}
-	case c != 0:
-		switch out {
-		case "F":
-			converted = conv.CelsiusToFahrenheit(c)
-		case "K":
-			converted = conv.CelsiusToKelvin(c)
-		case "C":
-			converted = c
-		}
-	case k != 0:
-		switch out {
-		case "F":
-			converted = conv.KelvinToFahrenheit(k)
-		case "C":
-			converted = conv.KelvinToCelsius(k)
-		case "K":
-			converted = k
-		}
-	}
-
-	switch out {
+	switch from {
 	case "C":
-		fmt.Printf("Output: %.2fK er %.2f°C\n", converted, c)
+		if to == "F" {
+			result = conv.CelsiusToFahrenheit(temp)
+		} else if to == "K" {
+			result = conv.CelsiusToKelvin(temp)
+		}
 	case "F":
-		fmt.Printf("Output: %.2fK er %.2f°F\n", converted, f)
+		if to == "C" {
+			result = conv.FahrenheitToCelsius(temp)
+		} else if to == "K" {
+			result = conv.FahrenheitToKelvin(temp)
+		}
 	case "K":
-		fmt.Printf("Output: %.2fK er %.2fK\n", converted, k)
+		if to == "C" {
+			result = conv.KelvinToCelsius(temp)
+		} else if to == "F" {
+			result = conv.KelvinToFahrenheit(temp)
+		}
 	}
 
-}
-
-func isFlagPassed(name string) bool {
-	found := false
-	flag.Visit(func(f *flag.Flag) {
-		if f.Name == name {
-			found = true
+	if result == 0 {
+		fmt.Println("Invalid input. Please check your source and target temperature scales.")
+	} else {
+		if result == float64(int(result)) {
+			fmt.Printf("%.0g degrees %s is %g degrees %s\n", temp, from, result, to)
+		} else {
+			fmt.Printf("%.3g degrees %s is %g degrees %s\n", temp, from, result, to)
 		}
-	})
-	return found
+	}
 }
