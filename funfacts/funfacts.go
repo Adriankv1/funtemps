575979a5a78018bbec5d2fe0dfbd9461778a7182
Adriankv1 <adriankv@uia.no>
2023-02-20 02:49:27 +0100

    Final update

--- funfacts/funfacts.go
+++ funfacts/funfacts.go
@@ -1,1 +1,52 @@
 package funfacts
+
+import (
+	"math/rand"
+	"time"
+)
+
+type FunFacts struct {
+	Sun   []string
+	Luna  []string
+	Terra []string
+}
+
+func GetFunFacts(about string) []string {
+	facts := FunFacts{
+		Sun: []string{
+			"Temperature in the Sun's core is 15,000,000 degrees Celsius",
+			"Temperature of the outer layer of the Sun is 5778 Kelvin",
+		},
+		Luna: []string{
+			"Temperature of the Moon's surface at night is -183 degrees Celsius",
+			"Temperature of the Moon's surface during the day is 106 degrees Celsius",
+		},
+		Terra: []string{
+			"Highest temperature measured on the Earth's surface is 134 degrees Fahrenheit, 56.7 degrees Celsius, 329.82 Kelvin",
+			"Lowest temperature measured on the Earth's surface is -89.4 degrees Celsius",
+			"Temperature in the Earth's inner core is 9392 Kelvin",
+		},
+	}
+
+	var factsList []string
+
+	switch about {
+	case "sun":
+		factsList = facts.Sun
+	case "luna":
+		factsList = facts.Luna
+	case "terra":
+		factsList = facts.Terra
+	default:
+		return factsList
+	}
+
+	// Sett opp en kilde for tilfeldige tall basert på nåværende tidspunkt
+	rand.Seed(time.Now().UnixNano())
+
+	// Velg en tilfeldig fun fact fra listen
+	index := rand.Intn(len(factsList))
+	fact := factsList[index]
+
+	return []string{fact}
+}
--- main.go
+++ main.go
@@ -4,6 +4,9 @@ import (
 	"flag"
 	"fmt"
 	"funtemps/v2/conv"
+	"funtemps/v2/funfacts"
+	"os"
+	"strings"
 )
 
 var from string
@@ -11,44 +14,75 @@ var to string
 var temp float64
 
 func init() {
-	flag.StringVar(&from, "from", "C", "source temperature scale (C/F/K)")
-	flag.StringVar(&to, "to", "F", "target temperature scale (C/F/K)")
+	flag.StringVar(&from, "from", "c", "source temperature scale (c/f/k)")
+	flag.StringVar(&to, "to", "f", "target temperature scale (c/f/k)")
 	flag.Float64Var(&temp, "temp", 0, "temperature to convert")
 }
 
 func main() {
+
 	flag.Parse()
 
-	var result float64
+	args := os.Args
 
-	switch from {
-	case "C":
-		if to == "F" {
-			result = conv.CelsiusToFahrenheit(temp)
-		} else if to == "K" {
-			result = conv.CelsiusToKelvin(temp)
-		}
-	case "F":
-		if to == "C" {
-			result = conv.FahrenheitToCelsius(temp)
-		} else if to == "K" {
-			result = conv.FahrenheitToKelvin(temp)
-		}
-	case "K":
-		if to == "C" {
-			result = conv.KelvinToCelsius(temp)
-		} else if to == "F" {
-			result = conv.KelvinToFahrenheit(temp)
-		}
-	}
+	// Sett opp en liste over gyldige verdier for "about"
+	aboutValues := []string{"sun", "luna", "terra"}
 
-	if result == 0 {
-		fmt.Println("Invalid input. Please check your source and target temperature scales.")
-	} else {
-		if result == float64(int(result)) {
-			fmt.Printf("%.0g degrees %s is %g degrees %s\n", temp, from, result, to)
+	// Sjekk om det er to argumenter og det andre argumentet er en gyldig verdi for "about"
+	if len(args) == 2 && contains(aboutValues, args[1]) {
+		// Hent ut en tilfeldig fun fact om valgt tema
+		facts := funfacts.GetFunFacts(args[1])
+
+		// Skriv ut fun facten
+		if len(facts) > 0 {
+			fmt.Println("Did you know? ", facts[0])
 		} else {
-			fmt.Printf("%.3g degrees %s is %g degrees %s\n", temp, from, result, to)
+			fmt.Println("No fun facts found for", args[1])
+		}
+	} else {
+		from = strings.ToUpper(from)
+		to = strings.ToUpper(to)
+		var result float64
+
+		switch from {
+		case "C":
+			if to == "F" {
+				result = conv.CelsiusToFahrenheit(temp)
+			} else if to == "K" {
+				result = conv.CelsiusToKelvin(temp)
+			}
+		case "F":
+			if to == "C" {
+				result = conv.FahrenheitToCelsius(temp)
+			} else if to == "K" {
+				result = conv.FahrenheitToKelvin(temp)
+			}
+		case "K":
+			if to == "C" {
+				result = conv.KelvinToCelsius(temp)
+			} else if to == "F" {
+				result = conv.KelvinToFahrenheit(temp)
+			}
+		}
+
+		if result == 0 {
+			fmt.Println("Invalid input. Please check your source and target temperature scales.")
+		} else {
+			if result == float64(int(result)) {
+				fmt.Printf("%.0g degrees %s is %g degrees %s\n", temp, from, result, to)
+			} else {
+				fmt.Printf("%.3g degrees %s is %g degrees %s\n", temp, from, result, to)
+			}
 		}
 	}
+}
+
+// Hjelpefunksjon som sjekker om en gitt streng finnes i en gitt liste
+func contains(slice []string, str string) bool {
+	for _, item := range slice {
+		if item == str {
+			return true
+		}
+	}
+	return false
 }
